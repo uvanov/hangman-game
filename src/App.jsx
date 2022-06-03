0752f4c1e973213beaf09cc39a471af8539d9d6d
uvanov <kirill.ivanov000@gmail.com>
2022-06-03 20:35:08 +0300

    Cotrols layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 // Import modules
 import React from 'react';
 import styled from '@emotion/styled';
-import { Center } from '@chakra-ui/react';
+
+// UI
+import {
+  Flex,
+  Center,
+  Input,
+  Button,
+  Stack
+} from '@chakra-ui/react';
 
 // Exports
 export const App = () => {
   return (
     <Center h='100%'>
-      App
+      <Flex
+        justifyContent='center'
+        alignItems='center'
+        w='60%'
+        h='80%'
+        bg='#7FC14D'
+        boxShadow='2xl'
+        borderRadius='20px'
+        pb={10}
+      >
+        <Stack
+          alignSelf='flex-end'
+          direction='row'
+          spacing='10px'
+          w='50%'
+        >
+          <Input
+            placeholder='Word'
+            size='lg'
+            bg='white'
+            boxShadow='lg'
+          />
+          <Button
+            colorScheme='teal'
+            size='lg'
+          >
+            Submit
+          </Button>
+        </Stack>
+      </Flex>
     </Center>
   );
 }
