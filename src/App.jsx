33faf4abe236b2c9435616bf453e61691769cf85
uvanov <kirill.ivanov000@gmail.com>
2022-06-03 19:37:22 +0300

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// Import modules
+import React from 'react';
+import styled from '@emotion/styled';
+import { Center } from '@chakra-ui/react';
+
+// Exports
+export const App = () => {
+  return (
+    <Center h='100%'>
+      App
+    </Center>
+  );
+}
