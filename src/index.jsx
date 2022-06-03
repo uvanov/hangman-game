5a20575b9c8f5d466c71b6139d375d647f0d43bf
uvanov <kirill.ivanov000@gmail.com>
2022-06-03 21:28:55 +0300

    Install redux / Create redux-store

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
+// Import modules
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
+
+// Local modules
 import { App } from './App'
 import './index.css';
 
+// Import Store
+import { store } from './store/store';
+
+// Render
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ChakraProvider>
 );
---
+++ src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// Create Store
+export const store = configureStore({
+  reducer: {}
+});
